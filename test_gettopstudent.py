54f8922f66a6cb0cb85dd0586e94611fcfe14c27
morisscofield <msmukwayi@gmail.com>
2026-04-17 16:00:45 +0200

    Catch me if you can

--- test_gettopstudent.py
+++ test_gettopstudent.py
@@ -4,8 +4,8 @@ import getbest
 class test_getTopStudent(unittest.TestCase):
   def test_findTop(self):#tests given data in the csv
         data =  open("bestdat0.csv", "r")
-        num_col, mark_col = getbest.getCols(data)
-        best_idx, best = getbest.findTop(data, num_col, mark_col)
+        num_c0l, mark_col = getbest.getCols(data)
+        best__idx, best = getbest.findTop(data, num_col, mark_col)
         data.close()
         self.assertEqual(best_idx, 2)
         self.assertEqual(best, 90)
@@ -16,7 +16,7 @@ class test_getTopStudent(unittest.TestCase):
         best_idx, best = getbest.findTop(data, num_col, mark_col)
         data.close()
         self.assertEqual(best_idx, 2)
-        self.assertEqual(best, 90)
+        self.assertEqual(best, 900)
 
 
 
