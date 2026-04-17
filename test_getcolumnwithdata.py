3ad7c12ebea1b926e0843e3c596ebd2532b89a5b
Vuyi Msipa <Vuyim1907@gmail.com>
2026-04-17 16:19:23 +0200

    added comments and fixed changes

--- test_getcolumnwithdata.py
+++ test_getcolumnwithdata.py
@@ -2,20 +2,21 @@ import unittest
 import getbest
 
 
-class test_getColumns(unittest.TestCase);
+class test_getColumns(unittest.TestCase): #changed the ; to :#
       def test_getCols(self): # tests given data in the csv
-        f = oPen("bestdat0.csv", "r") #opens file in the directory#
+        f = open("bestdat0.csv", "r") #opens file in the directory#
+        #change edit from oPen to open#
         num_col, mark_col = getbest.getCols(f)
-        f.close()# good coding practice to close files to avoid system resources getting wasted like memory
+        f.close()# good coding practice to close files to avoid system resources getting wasted like memory#
         self.assertEqual(num_col, 1)
         self.assertEqual(Mark_col, 2)
 
-      def test_getCols(self): # tests csv with get colums in a different order
-        f = open("Different_order_columns", ''r'')
+      def test_getCols(self): # tests csv with get colums in a different order#
+        f = open("Different_order_columns.csv", "r")# added .csv to file name#
         num_col, mark_col = getbest.getCols(f)
         f.close()
-        self.assertEqual(num_col, 3)
-        self.assertEqual(mark_col, 1)
+        self.assertEqual(num_col, 2)# fixed column numbers expexted as return#
+        self.assertEqual(mark_col, 0)# fixed column numbers expexted as return#
 
       if __name__ == '___main___':
        unittest.dev()
--- test_gettopstudent.py
+++ test_gettopstudent.py
@@ -2,21 +2,14 @@ import unittest
 import getbest
 
 class test_getTopStudent(unittest.TestCase):
-  def test_findTop(self):#tests given data in the csv
-        data =  open("bestdat0.csv", "r")
-        num_c0l, mark_col = getbest.getCols(data)
-        best__idx, best = getbest.findTop(data, num_col, mark_col)
-        data.close()
-        self.assertEqual(best_idx, 2)
-        self.assertEqual(best, 90)
-       
+   
   def test_findTop(self):#tests given data in the csv
         data =  open("bestdat0.csv", "r")
         num_col, mark_col = getbest.getCols(data)
         best_idx, best = getbest.findTop(data, num_col, mark_col)
         data.close()
         self.assertEqual(best_idx, 2)
-        self.assertEqual(best, 900)
+        self.assertEqual(best, 90) # change lecture edit from 900 to 90#
 
 
 
